2b8371f5d7d1b03f1569fd26440f5f1e71ac16cf
Micah Acinapura <micahnyc@gmail.com>
2019-03-05 16:04:08 -0500

    update compare script

--- OpenSpaceVisualTesting/TestGroups/targetcompare.bat
+++ OpenSpaceVisualTesting/TestGroups/targetcompare.bat
@@ -1,12 +1,15 @@
 ::targetcompare.bat
 @echo off
-setlocal enableDelayedExpansion
+setlocal enableDelayedExpansion 
+echo.>comparison.report
 for /R %%F in (Target*.png) do (
 	set _fullPath=%%F
 	set _fullPath=!_fullPath!
+	set _fileName=%%~nF
+	set _fileName=!_fileName!
 	call set _resultPath=%%_fullPath:Target=Result%%
 	call set _differencePath=%%_fullPath:Target=Difference%%
-	echo %%F:
-	compare.exe -fuzz 0.5%% -metric ae %%F !_resultPath! !_differencePath!
-	echo.
+	echo !_fileName!>>comparison.report
+	compare.exe -fuzz 0.5%% -metric ae %%F !_resultPath! !_differencePath! 2>>comparison.report
+	echo.>>comparison.report
 )
