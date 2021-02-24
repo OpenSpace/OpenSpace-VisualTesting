6ba034667b560f0f1f7c6ad040d80e6c02010de2
GPayne <payne.gene@gmail.com>
2021-02-24 12:15:35 -0700

    Added python scripts for linux and possibly Mac

---
+++ OpenSpaceVisualTesting/AssetTester.py
@@ -0,0 +1,138 @@
+#!/usr/bin/python3
+
+import sys
+import os
+import time
+import json
+import pathlib
+from glob import glob
+from subprocess import Popen, PIPE, STDOUT, check_output, CalledProcessError
+import subprocess
+import OpenSpaceSession as OSS
+
+#This script expects to run within the OpenSpaceVisualTesting repo cloned directory
+# structure, and also expects that this repo exists in the same directory as the
+# OpenSpace application repo cloned directory. Example:
+#
+# OpenSpace/
+#   apps/
+#   bin/
+#     RelWithDebInfo/ | Release/ | Debug/ (only in Windows?)
+#   cache/
+#   ...
+#   src/
+#   support/
+#   temp/
+#   tests/
+#     AssetLoaderTest/
+#     ...
+#     visual/ (visual tests exist here)
+# OpenSpaceVisualTesting/
+#   OpenSpaceVisualTesting/
+
+def checkForInstalledComponents():
+    p = Popen("wmctrl --version", shell=True, stdin=PIPE, stdout=PIPE, close_fds=True)
+    vCheck = p.stdout.read()
+    if len(vCheck) == 0:
+        print("Error: wmctrl does not appear to be installed (apt install wmctrl)")
+        quit(-1)
+    p = Popen("xdotool --version", shell=True, stdin=PIPE, stdout=PIPE, close_fds=True)
+    vCheck = p.stdout.read()
+    if len(vCheck) == 0:
+        print("Error: xdotool does not appear to be installed (apt install xdotool)")
+        quit(-1)
+
+def runAssetTests():
+    baseDirOpenSpace = "OpenSpace"
+    baseDirOpenSpaceVisualTesting = "OpenSpaceVisualTesting/OpenSpaceVisualTesting"
+    testDirName = "tests/visual"
+    baseTestDir, baseOsDir = figureOutDirs(testDirName, baseDirOpenSpaceVisualTesting, \
+        baseDirOpenSpace)
+    dirs = os.listdir(baseTestDir)
+    numTestCases = len(dirs)
+    testCaseNum = 1
+    for dir in dirs:
+        print("Test case " + str(testCaseNum) + "/" + str(numTestCases) + ":")
+        processTestDirectory(baseTestDir + "/" + dir, testDirName, baseOsDir)
+        testCaseNum = testCaseNum + 1
+
+def figureOutDirs(testDirName, baseDirVis, baseDirOs):
+    solutionDir = os.getcwd()
+    sDirIdx = solutionDir.rfind(baseDirVis)
+    commonBaseDir = solutionDir[0:sDirIdx]
+    openspaceDir = commonBaseDir + "/" + baseDirOs
+    fullOsPathTesting = commonBaseDir + "/" + baseDirVis + "/" + testDirName
+    return fullOsPathTesting, openspaceDir
+
+#Process all files in the directory passed in, recurse on any directories 
+#that are found, and process the files they contain.
+def processTestDirectory(targetDirectory, testDirName, baseOsDir):
+    idx = targetDirectory.rfind(testDirName) + len(testDirName) + 1
+    testGroup = targetDirectory[idx:999]
+    print("In processTestDirectory: ", end="")
+    print(targetDirectory)
+    fileEntries = pathlib.Path(targetDirectory).glob("*.ostest")
+    fileListing = list(fileEntries)
+    numFileEntries = len(fileListing)
+    feNum = 1
+    for fe in fileListing:
+        print("File entry " + str(feNum) + "/" + str(numFileEntries) + ":")
+        processTestFile(targetDirectory, fe, testGroup, baseOsDir)
+        feNum = feNum + 1
+
+#Insert logic for processing foundTestCases files here
+def processTestFile(targetDirectory, path, testGroup, baseOsDir):
+    print("Located in " + targetDirectory)
+    print("Starting Test file '" + path.name + "'")
+    numSlashes = path.name.count("/")
+    if numSlashes > 0:
+        path.name = path.name[path.name.rfind("/")]
+    scenarioName = path.name[0:path.name.find(".ostest")]
+    print("testName '" + testGroup + "." + scenarioName + "'")
+    print("Starting asset '" + testGroup + "'.")
+
+    ospace = OSS.OSSession(testGroup, baseOsDir + "/bin")
+    print("AssetTester: OpenSpace initialized")
+    ospace.startOpenSpace()
+    time.sleep(30)
+    print("AssetTester: Ready to start sending commands to OpenSpace.")
+    ospace.focusOpenSpaceWindow()
+    time.sleep(0.1)
+    ospace.toggleHudVisibility()
+    time.sleep(0.1)
+
+    with open(targetDirectory + "/" + scenarioName + ".ostest") as f:
+        data = json.load(f)
+        for d in data:
+            time.sleep(0.25)
+            if d["type"] == "script":
+                ospace.sendScript(d["value"])
+            elif d["type"] == "wait":
+                time.sleep(int(d["value"]))
+            elif d["type"] == "screenshot":
+                if len(d["value"]) < 1:
+                    d["value"] = scenarioName
+                ospace.moveScreenShot(testGroup, d["value"])
+            elif d["type"] == "time":
+                timeScript = "openspace.time.setTime(\"" + d["value"] + "\");"
+                ospace.sendScript(timeScript)
+            elif d["type"] == "keys":
+                ospace.keyboardKeystroke(d["value"])
+            elif d["type"] == "pause":
+                pauseScript = "openspace.time.setPause(" + d["value"] + ");";
+                ospace.sendScript(pauseScript);
+            elif d["type"] == "navigationstate":
+                navScript = "openspace.navigation.setNavigationState(" + d["value"] + ");"
+                ospace.sendScript(navScript)
+            elif d["type"] == "recording":
+                recordingScript = "openspace.sessionRecording.startPlayback('" + GetTestDir()
+                recordingScript += "/" + testGroup + "/" + d["value"] + ".osrecording')"
+                ospace.sendScript(recordingScript)
+    #ospace.killOpenSpace()
+    ospace.quitOpenSpace()
+    print("Processed test '" + path.name + "'.")
+    time.sleep(10)
+
+if __name__ == "__main__":
+    checkForInstalledComponents()
+    runAssetTests()
---
+++ OpenSpaceVisualTesting/OpenSpaceSession.py
@@ -0,0 +1,163 @@
+#!/usr/bin/python3
+
+import sys
+import os
+import time
+import signal
+from subprocess import Popen, PIPE, STDOUT, check_output, CalledProcessError
+import subprocess
+import json
+import pathlib
+from glob import glob
+from pathlib import Path
+import shlex
+
+#This script expects to run within the OpenSpaceVisualTesting repo cloned directory
+# structure, and also expects that this repo exists in the same directory as the
+# OpenSpace application repo cloned directory. Example:
+#
+# OpenSpace/
+#   apps/
+#   bin/
+#     RelWithDebInfo/ | Release/ | Debug/ (only in Windows?)
+#   cache/
+#   ...
+#   src/
+#   support/
+#   temp/
+#   tests/
+#     AssetLoaderTest/
+#     ...
+#     visual/ (visual tests exist here)
+# OpenSpaceVisualTesting/
+#   OpenSpaceVisualTesting/
+
+class OSSession:
+    def __init__(self, profileCL, baseOsDir):
+        self.basePath = baseOsDir
+        if len(profileCL) == 0:
+            self.profile = "default"
+        else:
+            self.profile = profileCL
+        self.OpenSpaceAppId = "OpenSpace"
+        self.configValues = "--config \""
+        self.configValues += "ScreenshotUseDate=false;ModuleConfigurations.Server={};"
+        self.configValues += "ModuleConfigurations.WebBrowser.Enabled=false;"
+        self.configValues += "ModuleConfigurations.WebGui={};"
+        self.configValues += "ModuleConfigurations.GlobeBrowsing.WMSCacheEnabled=true;"
+        self.configValues += "BypassLauncher=true;"
+        self.configValues += "Profile='" + self.profile + "'" + "\""
+        self.runCommand = self.basePath + "/" + self.OpenSpaceAppId + " " + self.configValues
+        self.osProcId = 0
+
+    def startOpenSpace(self):
+        self.osProcId = subprocess.Popen(shlex.split(self.runCommand))
+        print("OSS: Started OpenSpace instance with ID '" + str(self.osProcId.pid) + "'")
+
+    def killOpenSpace(self):
+        print("OSS: kill OpenSpace instance")
+        os.killpg(os.getpgid(int(self.osProcId.pid)), signal.SIGTERM)
+        time.sleep(4)
+
+    def quitOpenSpace(self):
+        print("OSS: quit OpenSpace instance")
+        self.focusOpenSpaceWindow()
+        self.keyboardKeystroke("Escape")
+        time.sleep(4)
+
+    def toggleHudVisibility(self):
+        self.keyboardTypeWithHold("shift", "Tab")
+
+    def focusOpenSpaceWindow(self):
+        foundWinId = ""
+        p = Popen("wmctrl -lG", shell=True, stdin=PIPE, stdout=PIPE, stderr=STDOUT, \
+            close_fds=True)
+        wmOutput = p.stdout.read()
+        lineArray = wmOutput.splitlines()
+        for line in lineArray:
+            elems = line.split()
+            if (len(elems) == 8) and (elems[7].decode("ASCII") == self.OpenSpaceAppId):
+                foundWinId = elems[0]
+                break
+        if foundWinId != "":
+            focusWindowCmd = "wmctrl -iR " + foundWinId.decode("ASCII")
+            os.system(focusWindowCmd)
+
+    def sendScript(self, script):
+        script = script.replace("\"", "\\\"")
+        time.sleep(1)
+        self.keyboardKeystroke("quoteleft")
+        self.keyboardType(script)
+        #Adjust delay according to length of string to type
+        time.sleep(len(script) / 50 + 0.25)
+        self.keyboardKeystroke("Return")
+        time.sleep(0.25)
+        self.keyboardKeystroke("quoteleft")
+        time.sleep(0.25)
+        
+    def keyboardHoldDownKey(self, key):
+        os.system("xdotool keydown " + key)
+
+    def keyboardReleaseKey(self, key):
+        os.system("xdotool keyup " + key)
+
+    def keyboardKeystroke(self, key):
+        os.system("xdotool key " + key)
+
+    def keyboardType(self, string):
+        cmdString = "xdotool type \"" + string + "\""
+        os.system(cmdString)
+
+    #Function that executes a single keypress (specified by 'keyPress' while holding
+    # down the key specified by 'keyHold'
+    def keyboardTypeWithHold(self, keyHold, keyPress):
+        cmdString = "xdotool keydown " + keyHold + " key " + keyPress + " keyup " + keyHold
+        os.system(cmdString)
+
+    def addAssetFile(self, scenarioGroup, scenarioName):
+        time.sleep(1)
+        filePath = "../../OpenSpaceVisualTesting/OpenSpaceVisualTesting/TestGroups/"
+        filePath += scenarioGroup + "/TestingAsset" + scenarioGroup + scenarioName
+        self.sendScript("openspace.asset.add('" + filePath + "');")
+        self.keyboardKeystroke("Return")
+        time.sleep(0.25)
+        self.keyboardKeystroke("quoteleft")
+        time.sleep(2)
+
+    def setTime(self, time):
+        self.sendScript("openspace.time.setTime('" + time + "');")
+        time.sleep(1)
+
+    def moveScreenShot(self, scenarioGroup, scenarioName):
+        time.sleep(1)
+        #self.keyboardType("openspace.takeScreenshot();")
+        self.keyboardKeystroke("F12")
+        time.sleep(10)
+        solutionDir = os.getcwd()
+        tmpPath = self.basePath + "/../" + "screenshots/OpenSpace_000000.png"
+        if not Path(tmpPath).is_file():
+            print("OSS: Screenshot was not successful. Expected to find '" + tmpPath + "'")
+            return
+        targetDir = solutionDir + "/TargetImages/linux/"
+        Path(targetDir).mkdir(parents=True, exist_ok=True)
+        if not Path(targetDir).is_dir():
+            print("Target dir for screenshots '" + targetDir + "' was not successfully created.")
+            return
+        targetFilename = "Result" + scenarioGroup + scenarioName + ".png"
+        moveToPath = targetDir + targetFilename
+        if os.path.isfile(moveToPath):
+            os.remove(moveToPath)
+        os.rename(tmpPath, moveToPath)
+
+def initialization():
+    print("Run OpenSpaceSession")
+
+if __name__ == "__main__":
+    print("called __main__")
+    initialization()
+    ospace = OSSession("default", "~/Desktop/OpenSpace")
+    ospace.startOpenSpace()
+    time.sleep(30)
+    ospace.focusOpenSpaceWindow()
+    ospace.toggleHudVisibility()
+
