b35433dfa85b00c83e9c867123c30cd211aa0557
GPayne <payne.gene@gmail.com>
2023-08-11 14:47:13 -0600

    Added option to start run at a specific test

--- OpenSpaceVisualTesting/testRun.py
+++ OpenSpaceVisualTesting/testRun.py
@@ -41,6 +41,11 @@ def parserInitialization():
                             help="Specify a custom test to run, instead of all by "\
                             "default. Provide in 'group/testFile.ostest' format.",
                             required=False, default="")
+        parser.add_argument("-s", "--start-at", dest="startAtTest",
+                            help="Does a full test run but only starting at the "\
+                            "test specified here, instead of at the beginning. "\
+                            "Provide in 'group/testFile.ostest' format.",
+                            required=False, default="")
     except ImportError:
         parser = None
     return parser
@@ -106,7 +111,7 @@ def listAllTestFiles(baseOsDir):
             if testFile.endswith(".ostest"):
                 fullName = os.path.abspath(os.path.join(root, testFile))
                 listing.append(fullName[len(testingDir)+1:])
-    return listing
+    return sorted(listing)
 
 
 #Runs one or more tests which are listed in string list arg $2, at the base OpenSpace
@@ -168,22 +173,35 @@ def clearBuildFlagToSignalTestCompletionToJenkins():
 #Run the test(s) using the specified OpenSpace installation directory
 # $1 - Full path to the OpenSpace directory to be used for the test
 #      (the executable is located at bin/OpenSpace relative to this path)
-# [$2] - Optional relative path to the specific test file to run. OpenSpace
-#        will only run once with this test. The path that this is relative to is
-#        tests/visual/ in the base OpenSpace directory (not this OpenSpaceVisualTesting
-#        repository directory). The path format is:  testFolder/testName.ostest
-def executeTests(openspaceDir, testRelPath):
+# $2 - Relative path to the test file to start at. Tests will run in sorted alphabetical
+#      order for a full run. If this is a non-empty string, then the test run will start
+#      at the beginning but skip all tests until this test is encountered, then will run
+#      it, and continue running the remainder of the tests (use this if a run crashed).
+#      The path that this is relative to is tests/visual/ in the base OpenSpace directory
+#      (not this OpenSpaceVisualTesting repository directory).
+#      The path format is:  testFolder/testName.ostest
+# $3 - Relative path to the only test file in the series that will be run. See above
+#      notes for details about the path
+def executeTests(openspaceDir, startAtTest, soleTest):
     setUpBuildDirectoryForRun(openspaceDir)
-    if testRelPath == "":
-        allTestsListed = listAllTestFiles(openspaceDir)
+    allTestsListed = listAllTestFiles(openspaceDir)
+    if startAtTest == "" and soleTest == "":
         logAndDisplayMsg(f"Start looping through all found .ostest files:")
         for i in range(0, len(allTestsListed)):
             logAndDisplayMsg(f"    {i+1} {allTestsListed[i]}")
         runAllTests(openspaceDir, allTestsListed)
         runComparisons("")
-    else:
-        runAllTests(openspaceDir, {testRelPath})
-        runComparisons(testRelPath)
+    elif startAtTest != "":
+        while len(allTestsListed):
+            if allTestsListed[0] != startAtTest:
+                allTestsListed.pop(0)
+            else:
+                break
+        runAllTests(openspaceDir, allTestsListed)
+        runComparisons("")
+    elif soleTest != "":
+        runAllTests(openspaceDir, {soleTest})
+        runComparisons(soleTest)
     createFilesystemLinksAtWebServerDirectory
 
 
@@ -199,6 +217,14 @@ def getPathFromJenkinsTriggerFile():
         print(f"Cannot find jenkins trigger file at {BuildFlag}")
         quit(-1)
 
+def verifyCustomFile(osDir, testFile, messagePrefix):
+    testPath = os.path.abspath(os.path.join(osDir, ImageTestingSubdirInOs, testFile))
+    if os.path.isfile(testPath):
+        logAndDisplayMsg(f"{messagePrefix} {testFile}.")
+    else:
+        errorMsg(f"Error: cannot find specified test file at {testPath}.")
+        quit(-1)
+
 
 if __name__ == "__main__":
     assert sys.version_info >= (3, 5), "Script requires Python 3.5+."
@@ -206,36 +232,33 @@ if __name__ == "__main__":
         os.remove(LogFile)
     parser = parserInitialization()
     args = handleArgumentParsing(parser)
+    customizedTest = False
     #Determine which installation directory for OpenSpace to run
     if not args.customDir.strip():
         args.customDir = getPathFromJenkinsTriggerFile()
-        customTest = False
     else:
+        customizedTest = True
         logAndDisplayMsg(f"Running test(s) on OpenSpace installation at " \
                          f"{args.customDir}.")
-        customTest = True
-    #Verify custom test directory to run (if specified)
-    if args.customTest != "":
-        testPath = os.path.abspath(os.path.join(args.customDir, ImageTestingSubdirInOs,\
-                                                args.customTest))
-        if os.path.isfile(testPath):
-            logAndDisplayMsg(f"Running manual test on OpenSpace installation "\
-                             f"(test {args.customTest}).")
-        else:
-            errorMsg(f"Error: cannot find specified test file at {testPath}.")
-            quit(-1)
-    if customTest:
+
+    if args.startAtTest != "":
+        verifyCustomFile(args.customDir, args.startAtTest, "Starting test file run at ")
+        customizedTest = True
+    elif args.customTest != "":
+        verifyCustomFile(args.customDir, args.customTest, "Running manual test file ")
+        customizedTest = True
+    if customizedTest:
         #Run a single test if a custom directory or specific test was named
         logAndDisplayMsg(f"Run tests from {args.customDir}.")
-        executeTests(args.customDir, args.customTest)
-        logAndDisplayMsg("Finished with one-off custom test run")
+        executeTests(args.customDir, args.startAtTest, args.customTest)
+        logAndDisplayMsg("Finished with customzed test run")
     else:
         #Loop to wait for Jenkins trigger to run tests
         logAndDisplayMsg("Waiting for Jenkins build-completion trigger...")
         while True:
             args.customDir = getPathFromJenkinsTriggerFile()
             if args.customDir.strip():
-                executeTests(args.customDir, "")
+                executeTests(args.customDir, "", "")
                 clearBuildFlagToSignalTestCompletionToJenkins()
                 logAndDisplayMsg("Continuing to wait for next Jenkins trigger...")
             time.sleep(10)
